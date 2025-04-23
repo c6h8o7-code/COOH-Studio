90f96f8a9ab621c728983a87a43237d95926881b
c6h8o7-code <pzh123gxy2023@163.com>
2025-04-23 18:40:13 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,24 +1,9 @@
 <?php
-session_start();
-header('Content-Type: text/html; charset=utf-8');
-
-// 获取表单提交的用户名和密码
-$username = isset($_POST['username']) ? trim($_POST['username']) : '';
-$password = isset($_POST['password']) ? trim($_POST['password']) : '';
-
-// 检查用户名和密码是否为空
-if (empty($username) || empty($password)) {
-echo "用户名或密码不能为空";
-exit;
+$file_path = "login.php";
+if (file_exists($file_path)) {
+$fp = fopen($file_path, "r");
+$str = fread($fp, filesize($file_path)); // 读取整个文件内容
+echo $str = str_replace("\r\n", "<br />", $str);
+fclose($fp); // 关闭文件指针
 }
-$result = $username
-
-if ($result) {
-echo "登录完成"
-} else {
-// 用户名或密码错误
-echo "用户名或密码错误";
-}
-
-$conn->close();
 ?>
