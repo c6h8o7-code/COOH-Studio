efcb94437e90a25d16d15e49250b2c6376609730
c6h8o7-code <pzh123gxy2023@163.com>
2025-04-23 18:44:31 +0800

    Delete login.php

--- login.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file_path = "login.php";
-if (file_exists($file_path)) {
-$fp = fopen($file_path, "r");
-$str = fread($fp, filesize($file_path)); // 读取整个文件内容
-echo $str = str_replace("\r\n", "<br />", $str);
-fclose($fp); // 关闭文件指针
-}
-?>
