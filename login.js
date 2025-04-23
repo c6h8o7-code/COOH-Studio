4f00ce225d0339490209a8c3e55746779c3581d3
c6h8o7-code <pzh123gxy2023@163.com>
2025-04-23 19:36:37 +0800

    Update login.js

--- login.js
+++ login.js
@@ -498,6 +498,6 @@ function int64add5(dst, a, b, c, d, e)
 }
 function login(){
   let username = document.querySelector("body > div.inputBox > input[type=text]:nth-child(1)").innerText;
-  let password = document.querySelector("body > div.inputBox > input[type=password]:nth-child(1)").innerText;
+  let password = document.querySelector("body > div.inputBox > input[type=password]:nth-child(2)").innerText;
   if(sha512[hex_sha512(username)] === hex_sha512(password)) console.log("login accept!")
 }
